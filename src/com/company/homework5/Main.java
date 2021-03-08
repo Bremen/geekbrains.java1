7ff4759d43d6c0a06ce8a4aa7672063f930140d5
Boris Remen <boris.remen@gmail.com>
2021-03-08 10:57:45 +0300

    complete homework 5

---
+++ src/com/company/homework5/Main.java
@@ -0,0 +1,9 @@
+package com.company.homework5;
+
+import static com.company.homework5.HomeWork5.createEmployeeArrayAndPrintEmploeeInfoWhoOlderAgeThreshold;
+
+public class Main {
+    public static void main(String[] args) {
+        createEmployeeArrayAndPrintEmploeeInfoWhoOlderAgeThreshold();
+    }
+}
