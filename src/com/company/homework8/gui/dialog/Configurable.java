c01b9ce809cef68d660438cb29295a3bbce9f15e
Boris Remen <boris.remen@gmail.com>
2021-03-23 09:29:31 +0300

    TicTacToe GUI

---
+++ src/com/company/homework8/gui/dialog/Configurable.java
@@ -0,0 +1,12 @@
+package com.company.homework8.gui.dialog;
+
+import com.company.homework8.enums.DotType;
+import com.company.project.train.enums.DotType;
+
+public interface Configurable {
+
+    DotType getPlayerType();
+
+    int getMapSize();
+
+}
